8ca0b5fe03df643e39a77d9f6358268e0954f54b
Song Wang <sonwang@microsoft.com>
2021-04-13 02:35:35 +0000

    update

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+# if it is cpu
+#pip install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+pip install transformers==4.1.0   
+pip install gitpython==3.1.11 rouge-score==0.0.4 sacrebleu==1.4.13
+pip install dialogbart
